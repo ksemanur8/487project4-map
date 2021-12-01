46d92a5bfca428b7f20f72603950129466f1d6a1
ksemanur <59751315+ksemanur8@users.noreply.github.com>
2021-12-01 00:01:17 -0500

    Tooltips working

--- scripts/main.js
+++ scripts/main.js
@@ -21,15 +21,50 @@ function handleData(data) {
                 lng: elt.longitude
             }
         })
+        
         var infoTip = new google.maps.InfoWindow({
             content:
               '<div id="minifo">' +
                 '<h3>' + elt.name + '</h3>' 
-                + '<p>' + elt.address + '</p></div>' 
+                + '<p>' + elt.address + 
+                '</p><a class="viewModal" onclick="generateModal(' + elt.id + ')">More Info</a></div>'
           });
           myMarker.addListener("click", () => {
             infoTip.open({ anchor: myMarker, map, shouldFocus: false });
           });
+          
     })
 }
+
+function generateModal(id) {
+    let placeName;
+    let placeAddress;
+    let placeDescription;
+    let placePhoto;
+
+    fetch("places.json")
+  .then((response) => response.json())
+  .then((json) => handleData(json))
+    data.forEach((elt) => {
+        if(elt.id == id) {
+            elt.name = placeName;
+            elt.address = placeAddress;
+            elt.description = placeDescription;
+            elt.photo = placePhoto;
+        }
+    })
+    new jBox('Modal', {
+        width: 300,
+        height: 300,
+        attach: '.viewModal',
+        title: 'My Modal Window',
+        content: '<div id="morefo">' +
+        '<h3>' + placeName + '</h3>' 
+        + '<img src="' + placePhoto + '"/>' + 
+        '<p>' + placeAddress + '</p>' + 
+        + '<p>' + placeDescription + '</p>' + 
+        '</div>'
+      })
+
+ };
 
